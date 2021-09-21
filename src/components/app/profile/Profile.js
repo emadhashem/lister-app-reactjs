eb1025a5f5b249d9eed910110cfef24e5f89d2ef
emad hashem <eali32028@gmail.com>
2021-09-21 23:30:55 +0200

    add some designs

--- src/components/app/profile/Profile.js
+++ src/components/app/profile/Profile.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import './profilestyles.scss'
 
-
-function ProfileComp({idprofile, user}) {
+function ProfileComp({ idprofile, user }) {
     const [isMine, setisMine] = useState(idprofile == user.id)
-    
+
     return (
-        <div>
-            this ProfileComp {idprofile}
+        <div className="profilecontainer__" >
+            <div className="top__container" >
+                <div className="avatar__username__container" ></div>
+                <div className="folwer__folwing__container" ></div>
+                {
+                    (!isMine) && <div className="flw__messg__btns__container" ></div>
+                }
+            </div>
+            <div className="down__container" ></div>
         </div>
     )
 }
-const mapStateToProps = ({user}) => ({user})
+const mapStateToProps = ({ user }) => ({ user })
 export default connect(mapStateToProps)(ProfileComp)
