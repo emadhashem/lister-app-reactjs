5f04a18aee06fade1aeac5c8853055a49ffe832e
emad hashem <eali32028@gmail.com>
2021-09-24 15:02:18 +0200

    add uplaod img

--- src/components/app/profile/Profile.js
+++ src/components/app/profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { connect } from 'react-redux'
 import './profilestyles.scss'
 import Avatar from '@mui/material/Avatar'
@@ -11,7 +11,8 @@ import { v4 } from 'uuid';
 import PostBodyComp from '../post/postbody/PostBodyComp';
 import { addPostApi, getAllPosts } from '../../../server/posts'
 import CircularProgress from '@mui/material/CircularProgress';
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import {storage} from '../../../server/firebase'
 
 
 function ProfileComp({ idprofile, user }) {
@@ -24,6 +25,11 @@ function ProfileComp({ idprofile, user }) {
 
     const [postsArr, setpostsArr] = useState([])
     const [loadingPosts, setloadingPosts] = useState(false)
+    const [userImg, setuserImg] = useState(null)
+    useEffect(() => {
+        console.log(userImg)
+    }, [userImg])
+
     useEffect(() => {
         setisMine(idprofile == user.id)
     }, [idprofile])
@@ -60,12 +66,67 @@ function ProfileComp({ idprofile, user }) {
 
 
     }
+    function choosefile() {
+        inputRef.current.click();
+    }
+    const inputRef = useRef(null)
+    async function getThefile(e) {
+        try {
+            e.stopPropagation();
+            e.preventDefault();
+            await uplaodUserImg(e.target.files[0])
+
+        } catch (errImg) {
+            // console.log(errImg);
+        }
+    }
+    async function readBlob(blob) {
+        const objectURL = URL.createObjectURL(blob);
+        return objectURL;
+    }
+
+    async function uplaodUserImg(file) {
+        
+        const uploadTask = storage.ref(`imgs/${user.id}`).put(file)
+        uploadTask.on('state_changed',
+            (snapshot) => { },
+            (error) => { console.log(error) },
+            async () => {
+                // Upload completed successfully, now we can get the download URL
+                console.log(await downloadImg())
+            }
+
+        )
+    }
+    async function downloadImg() {
+        const url = await Storage.ref('imgs')
+            .child(user.id)
+            .getDownloadURL()
+        return url
+    }
     return (
         <div className="profilecontainer__" >
             <div className="top__container" >
 
                 <div className="avatar__username__container" >
-                    <Avatar alt="img-profile" sx={{ width: 150, height: 150 }} />
+                    <div className="avatar__container" >
+                        <Avatar onClick={choosefile}
+                            alt="img-profile"
+                            src={userImg}
+                            sx={{ width: 150, height: 150, cursor: 'pointer' }} />
+                        {(userImg) && <span>
+                            <IconButton>
+                                <DeleteIcon color="error" fontSize="large" />
+                            </IconButton>
+                        </span>}
+                        <input
+                            type="file"
+                            id="file"
+                            ref={inputRef}
+                            onChange={getThefile}
+                            style={{ display: "none" }}
+                        />
+                    </div>
                     <h2>User Name</h2>
                 </div>
                 <div className="folwer__folwing__container" >
@@ -134,7 +195,7 @@ function ProfileComp({ idprofile, user }) {
                         ))
 
                     }
-                    {(loadingPosts == true) && <div className = "loading__container" >
+                    {(loadingPosts == true) && <div className="loading__container" >
                         <CircularProgress color="success" />
                     </div>}
                 </div>
