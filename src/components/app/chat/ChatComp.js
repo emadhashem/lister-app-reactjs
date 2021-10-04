61f26c25d410d8f5783bffd82262e213609a92a0
emad hashem <eali32028@gmail.com>
2021-10-04 22:27:50 +0200

    continue in chat app

--- src/components/app/chat/ChatComp.js
+++ src/components/app/chat/ChatComp.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import { getAllMsgs } from '../../../server/chat'
 import { socket as io } from '../../../server/socket'
 function ChatComp({user}) {
     const [newMeassge, setnewMeassge] = useState('')
+    const [errMsg, seterrMsg] = useState('')
     const [txtMessage, settxtMessage] = useState('')
     useEffect(() => {
-        io.emit("join", {name : user.name, room : 'test', id : user.id})
+        
+        (async () => {
+            io.emit("join", {name : user.name, room : 'test', id : user.id})
+            const res = await getAllMsgs("test")
+            console.log(res)
+        })()
+        
         return () => {
             io.off()
         }
     }, [])
     useEffect(() => {
         io.on('recive_message', (userId, text) => {
-            console.log(text)
-            setnewMeassge(text)
+            setnewMeassge(text )
+        } )
+        io.on('error_msg', ( text) => {
+            seterrMsg(text )
         } )
     }, [])
     function sendMessage() {
@@ -25,6 +35,7 @@ function ChatComp({user}) {
                 onChange={ev => settxtMessage(ev.target.value)} />
             <input type="button" value="send" onClick = {sendMessage} />
             <div>{newMeassge}</div>
+            <div>{errMsg}</div>
         </div>
     )
 }
---
+++ src/server/chat.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+export async function removeMsg(msgId = "", roomName = "") {
+    try {
+        const res = await axios({
+            method: 'post',
+            url: 'http://localhost:4000/api/removemsg',
+            data: {
+                roomReq: roomName,
+                msgid: msgId,
+            },
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+        return res
+    } catch (error) {
+        return { error }
+    }
+}
+
+export async function getAllMsgs(room) {
+    try {
+        const res = await axios({
+            method: 'get',
+            url: 'http://localhost:4000/api/allmsgs/'+room,
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+        return res
+    } catch (error) {
+        return { error }
+    }
+}
