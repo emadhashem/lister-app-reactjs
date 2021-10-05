c8009d827fdf4721de35f72511b6fe748e696a6a
emad hashem <eali32028@gmail.com>
2021-10-05 18:57:09 +0200

    save the recent chat design

--- src/components/app/chat/ChatComp.js
+++ src/components/app/chat/ChatComp.js
@@ -1,19 +1,27 @@
+import { CircularProgress } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { getAllChats } from '../../../server/chat'
 import './chatstyles.scss'
+import RecentChatCompUser from './RecentChatCompUser'
+
+const tstUser = 'rMvSJOCOxxf6s54lHD9vFZK0sll2'
 function ChatComp({ user }) {
-    const [chats, setchats] = useState([])
+    const [chats, setchats] = useState(Array(20).fill(tstUser))
+    const [loadingChats, setloadingChats] = useState(false)
     useEffect(() => {
         (async () => {
+            setloadingChats(true)
             await getTheChats()
+            setloadingChats(false)
+            
         })()
     }, [])
 
     async function getTheChats() {
         try {
             const res = await getAllChats(user.id)
-            setchats(res.data.chaters)
+            // setchats(res.data.chaters)
         } catch (error) {
             alert('please wait there is kind of bug')
         }
@@ -24,9 +32,18 @@ function ChatComp({ user }) {
             <h1 className = "title__" >
                 Recent Chats
             </h1>
-            <div className = "chats__" >
-                
+            {(loadingChats == false) ? <div className = "chats__" >
+                {
+                    [...chats].map((item) => (
+                        <RecentChatCompUser key = {Math.random() * 100} userId = {item} />
+                        
+                    ))
+                }
+            </div> : <div style = {{width : 'auto', margin : 'auto', height : 'auto'}} >
+                <CircularProgress color = "info" />
             </div>
+            
+        }
         </div>
     )
 }
--- src/components/app/chat/RecentChatCompUser.js
+++ src/components/app/chat/RecentChatCompUser.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Avatar, CircularProgress } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router'
+import { downloadImg } from '../../../server/storageFirebase'
+import { get_the_user_data } from '../../../server/user'
+import './recentchatusercompstyles.scss'
+function RecentChatCompUser({ userId = '' }) {
+    const go = useHistory()
 
-function RecentChatCompUser({chater = ''}) {
+    const [usrName, setusrName] = useState('')
+    const [usrImg, setusrImg] = useState('')
+    const [loading, setloading] = useState(false)
+    useEffect(() => {
+        (async () => {
+            setloading(true)
+            await getUserData()
+            await getUserImg()
+            setloading(false)
+        })()
+    }, [])
+    async function getUserData() {
+        const res = await get_the_user_data(userId)
+        setusrName(res.fName + " " + res.lName)
+    }
+    async function getUserImg() {
+        const url = await downloadImg(`imgs/${userId}`)
+        setusrImg(url)
+    }
+    async function handleGoToChat() {
+        go.push('/member/chat/room/' + userId)
+    }
     return (
-        <div>
-            
+        <div className="chat__" onClick={handleGoToChat} >
+            {(loading == false) ? <div>
+                <Avatar src={usrImg} sx={{ width: 70, height: 70 }} />
+                <h3>{usrName}</h3>
+            </div> : <CircularProgress color="info" />
+
+            }
         </div>
     )
 }
